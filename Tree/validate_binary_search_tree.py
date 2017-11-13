9c6b2a150c0bfa9f5add24e638b52527905c34a3
Min Lin <lim1202@gmail.com>
2017-11-13 16:43:11 +0800

    add validate binary search tree.

---
+++ Tree/validate_binary_search_tree.py
@@ -0,0 +1,33 @@
+"""Validate Binary Search Tree"""
+# Given a binary tree, determine if it is a valid binary search tree (BST).
+# Assume a BST is defined as follows:
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+import sys
+from binarytree import convert, show
+
+
+def is_valid_bst(root):
+    """Return is a valid BST"""
+    return valid(root, 0, sys.maxsize)
+
+
+def valid(node, min_value, max_value):
+    """Iteratively validate BST"""
+    if node is None:
+        return True
+
+    if node.value <= min_value or node.value >= max_value:
+        return False
+
+    return valid(node.left, min_value, node.value) and valid(node.right, node.value, max_value)
+
+
+if __name__ == '__main__':
+    TREE1 = convert([7, 3, 2, 6, 9, 4, 1])
+    show(TREE1)
+    print("Is a valid binary search tree?", is_valid_bst(TREE1))
+    TREE2 = convert([4, 2, 6, 1, 3, 5, 7])
+    show(TREE2)
+    print("Is a valid binary search tree?", is_valid_bst(TREE2))
