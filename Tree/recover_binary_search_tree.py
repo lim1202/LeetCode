583349444cf45d80178fe74bc8c4924e3e21d129
Min Lin <lim1202@gmail.com>
2017-11-15 17:28:58 +0800

    add recover binary search tree script.

---
+++ Tree/recover_binary_search_tree.py
@@ -0,0 +1,58 @@
+"""Recover Binary Search Tree"""
+# Two elements of a binary search tree (BST) are swapped by mistake.
+# Recover the tree without changing its structure.
+# Note:`
+# A solution using O(n) space is pretty straight forward.
+# Could you devise a constant space solution?
+from binarytree import convert, show
+
+
+def recover_tree(root):
+    """Recover the binary search tree"""
+    found = False
+    pre_cur = None
+    pre1 = None
+    pre2 = None
+
+    if root is None:
+        return
+
+    cur = root
+    while cur is not None:
+        if cur.left is None:
+            if pre_cur and pre_cur.value > cur.value:
+                if not found:
+                    pre1 = pre_cur
+                    found = True
+                pre2 = cur
+
+            pre_cur = cur
+            cur = cur.right
+        else:
+            pre = cur.left
+            while pre.right and pre.right.value != cur.value:
+                pre = pre.right
+            if pre.right is None:
+                pre.right = cur
+                cur = cur.left
+            else:
+                if pre_cur.value > cur.value:
+                    if not found:
+                        pre1 = pre_cur
+                        found = True
+                    pre2 = cur
+                pre_cur = cur
+                pre.right = None
+                cur = cur.right
+
+        if pre1 and pre2:
+            temp = pre1.value
+            pre1.value = pre2.value
+            pre2.value = temp
+
+
+if __name__ == "__main__":
+    TREE1 = convert([4, 2, 7, 1, 3, 6, 5])
+    show(TREE1)
+    recover_tree(TREE1)
+    show(TREE1)
