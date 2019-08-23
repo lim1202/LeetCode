2df293ea73256343971bbc820c576e26d89135b8
lim <lim1202@gmail.com>
2019-08-23 11:41:25 +0800

    add merge sort algorithm.

--- Algorithm/sort.py
+++ Algorithm/sort.py
@@ -3,12 +3,11 @@
 import random
 import time
 
-def bubble_sort(unsorted_list):
-    """Bubble Sort"""
-    if not unsorted_list:
-        return None
 
-    nums = unsorted_list.copy()
+def bubble_sort(nums):
+    """Bubble Sort"""
+    if not nums:
+        return None
 
     for i in range(len(nums)):
         flag = False
@@ -18,14 +17,13 @@ def bubble_sort(unsorted_list):
                 flag = True
         if not flag:
             break
-    return nums
+    return
 
-def insertion_sort(unsorted_list):
+
+def insertion_sort(nums):
     """Insertion Sort"""
-    if not unsorted_list:
+    if not nums:
         return None
-
-    nums = unsorted_list.copy()
 
     for i in range(1, len(nums)):
         value = nums[i]
@@ -38,19 +36,75 @@ def insertion_sort(unsorted_list):
             j = j - 1
         nums[j + 1] = value
 
-    return nums
+    return
+
+
+def merge_sort(nums):
+    """Merge Sort"""
+    if not nums:
+        return None
+
+    def merge(C, p, q, r):
+        A = C[p:q+1]
+        B = C[q+1:r+1]
+        i = r
+        j = len(A) - 1
+        k = len(B) - 1
+
+        while i >= p:
+            if (j >= 0 and k >= 0):
+                if A[j] > B[k]:
+                    C[i] = A[j]
+                    j -= 1
+                else:
+                    C[i] = B[k]
+                    k -= 1
+            elif j >= 0:
+                C[i] = A[j]
+                j -= 1
+            elif k >= 0:
+                C[i] = B[k]
+                k -= 1
+            i -= 1
+
+        return
+
+    def merge_sort_c(C, p, r):
+        if p >= r:
+            return
+
+        q = (p+r)//2
+
+        merge_sort_c(C, p, q)
+        merge_sort_c(C, q+1, r)
+        merge(C, p, q, r)
+
+        return
+
+    merge_sort_c(nums, 0, len(nums)-1)
+
+    return
+
 
 if __name__ == "__main__":
     RANDOM_LIST = [i for i in range(1, 11)]
     random.shuffle(RANDOM_LIST)
     print('Test array: {}'.format(RANDOM_LIST))
 
+    BS_LIST = RANDOM_LIST.copy()
     start_time = time.time()
-    BS_LIST = bubble_sort(RANDOM_LIST)
+    bubble_sort(BS_LIST)
     print('Bubble Sort result: {}'.format(BS_LIST))
     print('Duration: {} ms'.format((time.time() - start_time) * 1000))
 
+    IS_LIST = RANDOM_LIST.copy()
     start_time = time.time()
-    IS_LIST = insertion_sort(RANDOM_LIST)
+    insertion_sort(IS_LIST)
     print('Insertion Sort result: {}'.format(IS_LIST))
+    print('Duration: {} ms'.format((time.time() - start_time) * 1000))
+
+    MS_LIST = RANDOM_LIST.copy()
+    start_time = time.time()
+    merge_sort(MS_LIST)
+    print('Merge Sort result: {}'.format(MS_LIST))
     print('Duration: {} ms'.format((time.time() - start_time) * 1000))
